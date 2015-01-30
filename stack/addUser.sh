4ebdf79b21d0636041b347dc9acd16a8f89f6f7b
clark21 <clark@dev-engine.net>
2015-01-30 18:01:30 +0800

    add stack script

---
+++ stack/addUser.sh
@@ -0,0 +1,10 @@
+pass=$(perl -e 'print crypt($ARGV[0], "password")', $3)
+
+expect -c "
+spawn ssh $5@$1
+expect -re \".*?assword.*?\"
+send \"$2\n\"
+expect -re \".*?root.*?\"
+send \"useradd -M -p $pass $4\n\"
+send \"echo DONE!!!\n\"
+expect -re \"DONE!!!\""
